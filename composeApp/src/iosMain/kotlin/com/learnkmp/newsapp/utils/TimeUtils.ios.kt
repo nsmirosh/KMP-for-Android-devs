b0af34c090c3504966ded3c976958c807531290d
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2025-01-22 19:13:36 +0700

    iOS solution implemented as well

--- composeApp/src/iosMain/kotlin/com/learnkmp/newsapp/utils/TimeUtils.ios.kt
+++ composeApp/src/iosMain/kotlin/com/learnkmp/newsapp/utils/TimeUtils.ios.kt
@@ -1,5 +1,23 @@
 package com.learnkmp.newsapp.utils
+import platform.Foundation.NSDate
+import platform.Foundation.NSDateFormatter
+import platform.Foundation.NSLocale
+import platform.Foundation.NSTimeZone
+import platform.Foundation.currentLocale
+import platform.Foundation.localTimeZone
 
 actual fun formatCurrentTimeStamp(timeStamp: String): String {
-    TODO("Not yet implemented")
+    val inputFormmater = NSDateFormatter()
+    inputFormmater.dateFormat = "yyyy-MM-dd HH:mm:ss"
+    inputFormmater.locale = NSLocale.currentLocale()
+    inputFormmater.timeZone = NSTimeZone.localTimeZone()
+    val date: NSDate = try {
+        inputFormmater.dateFromString(timeStamp)!!
+    } catch (e: Exception) {
+        println("Error: $e")
+        NSDate()
+    }
+    val outputFormatter = NSDateFormatter()
+    outputFormatter.dateFormat = "MMM dd, yyyy hh:mm a"
+    return outputFormatter.stringFromDate(date)
 }
