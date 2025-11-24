fdcd099fbe6599c91f35d4b2b8907598a2f21ce8
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2025-11-24 16:13:01 +0700

    Added deps

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -64,6 +64,8 @@ kotlin {
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.serialization.kotlinx.json)
             implementation(libs.ktor.client.logging)
+            implementation(libs.coil.compose)
+            implementation(libs.coil.networking)
         }
     }
 }
