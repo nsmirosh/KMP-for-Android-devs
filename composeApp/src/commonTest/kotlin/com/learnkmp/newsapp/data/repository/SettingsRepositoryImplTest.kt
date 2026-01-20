f9df3b9d41c99d5bf4458972627846a1598b0795
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2026-01-20 18:26:01 +0700

    implemented tests with Koin

--- composeApp/src/commonTest/kotlin/com/learnkmp/newsapp/data/repository/SettingsRepositoryImplTest.kt
+++ composeApp/src/commonTest/kotlin/com/learnkmp/newsapp/data/repository/SettingsRepositoryImplTest.kt
@@ -1,23 +1,42 @@
 package com.learnkmp.newsapp.data.repository
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import com.learnkmp.newsapp.di.repositoryModule
 import com.learnkmp.newsapp.domain.model.Category
 import com.learnkmp.newsapp.domain.model.Result
 import com.learnkmp.newsapp.domain.repository.SettingsRepository
 import kotlinx.coroutines.test.runTest
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
+import org.koin.test.KoinTest
+import org.koin.test.inject
+import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
-class SettingsRepositoryImplTest {
+class SettingsRepositoryImplTest : KoinTest {
 
-    private lateinit var repository: SettingsRepository
+    private val repository: SettingsRepository by inject()
+
+    private fun testModule() = module {
+        single<DataStore<Preferences>> { FakeDataStore() }
+    }
 
     @BeforeTest
     fun setup() {
-        val fakeDataStore = FakeDataStore()
-        repository = SettingsRepositoryImpl(fakeDataStore)
+        startKoin {
+            modules(testModule() + repositoryModule)
+        }
+    }
+
+    @AfterTest
+    fun tearDown() {
+        stopKoin()
     }
 
     @Test
@@ -28,7 +47,7 @@ class SettingsRepositoryImplTest {
     }
 
     @Test
-    fun `saveSelectedCategory() saves the category`() = runTest {
+    fun `saveSelectedCategory saves the category`() = runTest {
         repository.saveSelectedCategory(Category.BUSINESS)
         val result = repository.getSelectedCategory()
         assertTrue(result is Result.Success)
@@ -36,7 +55,7 @@ class SettingsRepositoryImplTest {
     }
 
     @Test
-    fun `saveSelectedCategory() with null removes the category`() = runTest {
+    fun `saveSelectedCategory with null removes the category`() = runTest {
         repository.saveSelectedCategory(Category.BUSINESS)
         repository.saveSelectedCategory(null)
         val result = repository.getSelectedCategory()
