aef75f835a1241eab9cc852e89770ed8d5133b0c
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2026-02-08 18:58:27 +0700

    Updated dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -68,7 +68,7 @@ kotlin {
             implementation(libs.coil.networking)
             implementation(libs.androidx.extended.icons)
 
-            implementation("org.jetbrains.compose.ui:ui-tooling-preview:1.10.0-rc02")
+            implementation(libs.ui.tooling.preview)
             implementation(libs.androidx.nav3.ui)
         }
     }
