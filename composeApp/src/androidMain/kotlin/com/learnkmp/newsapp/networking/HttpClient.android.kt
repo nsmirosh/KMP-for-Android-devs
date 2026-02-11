1ae1c2a600a62bab9a32aa36accf2c306a46e869
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2026-02-11 20:01:05 +0700

    Removed comments

--- composeApp/src/androidMain/kotlin/com/learnkmp/newsapp/networking/HttpClient.android.kt
+++ composeApp/src/androidMain/kotlin/com/learnkmp/newsapp/networking/HttpClient.android.kt
@@ -4,6 +4,4 @@ import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 
 
-//TODO define your Android actual here
-
-actual fun createPlatformHttpClient(): HttpClient  = HttpClient(OkHttp)
+actual fun createPlatformHttpClient(): HttpClient = HttpClient(OkHttp)
