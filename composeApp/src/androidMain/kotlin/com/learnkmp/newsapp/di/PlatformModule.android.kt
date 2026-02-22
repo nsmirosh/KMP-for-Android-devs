25db66944666fa034f3f15b5d7b4b43edba60e03
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2026-02-22 16:14:45 +0700

    Removed unneccessary dependency

--- composeApp/src/androidMain/kotlin/com/learnkmp/newsapp/di/PlatformModule.android.kt
+++ composeApp/src/androidMain/kotlin/com/learnkmp/newsapp/di/PlatformModule.android.kt
@@ -1,9 +1,13 @@
 package com.learnkmp.newsapp.di
 
 import org.koin.dsl.module
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import com.learnkmp.newsapp.data.createAndroidDataStore
+import com.learnkmp.newsapp.database.getAndroidDatabaseBuilder
+import org.koin.android.ext.koin.androidContext
 
 actual fun platformModule() = module {
-    includes(
-        androidAppModule(),
-    )
+    single<DataStore<Preferences>> { createAndroidDataStore(androidContext()) }
+    single { getAndroidDatabaseBuilder(androidContext()) }
 }
--- composeApp/src/commonMain/kotlin/com/learnkmp/newsapp/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/com/learnkmp/newsapp/di/AppModule.kt
@@ -1,6 +1,5 @@
 package com.learnkmp.newsapp.di
 
-import com.learnkmp.newsapp.database.AppDatabase
 import com.learnkmp.newsapp.database.getRoomDatabase
 import com.learnkmp.newsapp.networking.buildHttpClient
 import com.learnkmp.newsapp.ui.ArticleViewModel
@@ -9,13 +8,9 @@ import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 
-val databaseModule = module {
-    single { getRoomDatabase(get()) }
-    single { get<AppDatabase>().articleDao() }
-}
-
 fun appModule() = module {
-    includes(databaseModule)
+    single { getRoomDatabase(get()).articleDao()  }
+    single<HttpClient> { buildHttpClient() }
     single<HttpClient> { buildHttpClient() }
     viewModelOf(::ArticleViewModel)
 }
